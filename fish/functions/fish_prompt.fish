dcb9ac2a608bcf5524f5391a481ec78b4cadbad9
Jacob Peddicord <jacob@peddicord.net>
2024-02-28 06:52:46 -0800

    fish color tinkering

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -10,14 +10,14 @@ function fish_prompt
 
     # ssh prefix
     test $SSH_TTY
-    and echo -n (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+    and echo -n (set_color 777777)$USER'@'(set_color 0883ae)(prompt_hostname)' '
 
     # root prefix
     test "$USER" = 'root'
-    and echo -n (set_color red)'# '
+    and echo -n (set_color bf4100)'# '
 
     # directory
-    echo -n (set_color 129999)(prompt_pwd)' '
+    echo -n (set_color bf5e00)(prompt_pwd)' '
 
     # git
     echo -n (__fish_git_prompt '(%s) ')
@@ -25,9 +25,9 @@ function fish_prompt
     # duration
     if test "$CMD_DURATION" -gt 500
         if test "$CMD_DURATION" -gt 60000
-            printf (set_color -d magenta)'%.0fm%.0fs' (math $CMD_DURATION / 1000 / 60) (math $CMD_DURATION / 1000 % 60)
+            printf (set_color -d aaaaaa)'%.0fm%.0fs' (math $CMD_DURATION / 1000 / 60) (math $CMD_DURATION / 1000 % 60)
         else
-            printf (set_color -d magenta)'%.2fs' (math $CMD_DURATION / 1000)
+            printf (set_color -d aaaaaa)'%.2fs' (math $CMD_DURATION / 1000)
         end
         set_color normal
     end
@@ -35,8 +35,8 @@ function fish_prompt
     # command line
     echo
     if test $cmd_status -eq 0
-        echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+        echo -n (set_color 0cbdfb)'❯❯❯ '
     else
-        echo -n (set_color red)'❯❯❯ '
+        echo -n (set_color bf4100)'❯❯❯ '
     end
 end
