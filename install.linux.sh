d77e526a2f583b1e9b3cf2a612c15047c739db3e
Jacob Peddicord <jacob@peddicord.net>
2025-06-01 10:39:48 -0700

    use Brewfiles

--- install.linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-if [[ $(uname) != "Linux" ]]; then
-  exit 0
-fi
-
-brew install btop carapace mise neovim nushell ripgrep starship yazi
--- install.macos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-if [[ $(uname) != "Darwin" ]]; then
-  exit 0
-fi
-
-brew install btop carapace mise neovim nushell ripgrep starship yazi
