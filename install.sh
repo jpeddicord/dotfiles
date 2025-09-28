e1b83a4072988d75c472657844dd2f295c02fdc1
Jacob Peddicord <jacob@peddicord.net>
2025-09-28 11:31:27 -0700

    add some system tasks ('zzz')

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ cd "$(dirname $0)"
 which mise || ./setup/mise.run.sh
 export PATH="$HOME/.local/bin:$PATH"
 
+[ -e ~/.config/mise ] || ln -s "$PWD/files/_config/mise" ~/.config/mise
 mkdir -p ~/.config/mise/conf.d
 touch ~/.config/mise/config.toml  # mise will write to the first config file it sees
 ln -sf "$(realpath mise.global.toml)" ~/.config/mise/conf.d/dotfiles.toml  # and it shouldn't touch this one
