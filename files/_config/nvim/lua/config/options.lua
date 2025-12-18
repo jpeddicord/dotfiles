11b0f9ab1f3621993e89f83ba79d208a169188ab
Jacob Peddicord <jacob@peddicord.net>
2025-12-18 12:40:23 -0800

    neovim: set better snacks colors

--- files/_config/nvim/lua/config/options.lua
+++ files/_config/nvim/lua/config/options.lua
@@ -6,3 +6,9 @@ vim.opt.wrap = true
 vim.opt.relativenumber = false
 
 vim.g.snacks_animate = false
+
+-- better colors for snacks explorer
+vim.api.nvim_set_hl(0, "SnacksPickerDir", { link = "Text" })
+vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { link = "Text" })
+vim.api.nvim_set_hl(0, "SnacksPickerPathIgnored", { link = "Comment" })
+vim.api.nvim_set_hl(0, "SnacksPickerGitStatusUntracked", { link = "Special" })
